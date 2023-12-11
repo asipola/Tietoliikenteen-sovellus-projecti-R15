a6b2a3eb3cfa951f9a799818ba0df5618ed48a5e
asipola <aleksisipola@gmail.com>
2023-12-11 11:42:42 +0200

    beter gitignore

--- MatrixCreation/build/hci_rpmsg/modules/open-amp/open-amp/lib/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/ncs/v2.5.0/modules/lib/open-amp/open-amp/lib
-# Build directory: C:/Users/aleks/Documents/GitHub/Tietoliikenteen-sovellus-projecti-R15/BluetoothAccelerator/build/hci_rpmsg/modules/open-amp/open-amp/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("virtio")
-subdirs("rpmsg")
-subdirs("remoteproc")
--- MatrixCreation/build/image_preload.cmake
+++
@@ -1,7 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf5340dk_nrf5340_cpuapp" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "c:/Users/aleks/Documents/GitHub/Tietoliikenteen-sovellus-projecti-R15/BluetoothAccelerator/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "c:/Users/aleks/Documents/GitHub/Tietoliikenteen-sovellus-projecti-R15/BluetoothAccelerator/nrf5340dk_nrf5340_cpuapp_ns.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/c57af46cb7/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
