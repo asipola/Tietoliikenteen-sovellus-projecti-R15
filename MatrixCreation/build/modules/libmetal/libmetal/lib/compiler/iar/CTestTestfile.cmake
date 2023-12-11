5f5a9621f2d8ce0b6a0699f1447dea88a44c8595
asipola <aleksisipola@gmail.com>
2023-12-11 11:36:04 +0200

    somewhat final changes

---
+++ MatrixCreation/build/modules/libmetal/libmetal/lib/compiler/iar/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/ncs/v2.5.0/modules/hal/libmetal/libmetal/lib/compiler/iar
+# Build directory: C:/Users/aleks/Documents/GitHub/Tietoliikenteen-sovellus-projecti-R15/BluetoothAccelerator/build/modules/libmetal/libmetal/lib/compiler/iar
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
